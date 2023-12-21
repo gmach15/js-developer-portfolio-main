c1f9387cd8b16f9b24f86b544f90909f0365c1a9
Gabriel Machado <139392637+gmach15@users.noreply.github.com>
2023-12-21 01:28:39 -0300

    Troca de paths da API

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData () {
-    const url = 'https://raw.githubusercontent.com/gmach15/js-developer-portfolio/main/data/profile.json'
+    const url = 'https://raw.githubusercontent.com/gmach15/js-developer-portfolio-main/main/data/profile.json'
     const fetching = await fetch(url)
     return await fetching.json() 
 }
